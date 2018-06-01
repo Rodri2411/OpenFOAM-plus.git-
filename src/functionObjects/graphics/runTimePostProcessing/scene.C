demarle/openfoamvismodule

ENH: Consistency improvement for setting postProcessing directory name

src/runTimePostProcessing/scene.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2015-2017 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2015-2018 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -390,9 +390,17 @@ void Foam::functionObjects::runTimePostPro::scene::saveImage
 
     const Time& runTime = obr_.time();
 
-    fileName prefix(Pstream::parRun() ?
-        runTime.path()/".."/"postProcessing"/name_/obr_.time().timeName() :
-        runTime.path()/"postProcessing"/name_/obr_.time().timeName());
+    const fileName relPath
+    (
+        functionObject::outputPrefix/name_/obr_.time().timeName()
+    );
+
+    fileName prefix
+    (
+        Pstream::parRun() ?
+            runTime.path()/".."/relPath
+          : runTime.path()/relPath
+    );
 
     mkDir(prefix);
 

Rodri2411/OpenFOAM-plus.git-

src/functionObjects/graphics/runTimePostProcessing/scene.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015-2017 OpenCFD Ltd.
+     \\/     M anipulation  | Copyright (C) 2015-2018 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -389,9 +389,17 @@ void Foam::functionObjects::runTimePostPro::scene::saveImage
 
     const Time& runTime = obr_.time();
 
-    fileName prefix(Pstream::parRun() ?
-        runTime.path()/".."/"postProcessing"/name_/obr_.time().timeName() :
-        runTime.path()/"postProcessing"/name_/obr_.time().timeName());
+    const fileName relPath
+    (
+        functionObject::outputPrefix/name_/obr_.time().timeName()
+    );
+
+    fileName prefix
+    (
+        Pstream::parRun() ?
+            runTime.path()/".."/relPath
+          : runTime.path()/relPath
+    );
 
     mkDir(prefix);
 